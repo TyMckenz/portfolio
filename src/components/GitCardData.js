1f90e33c0aa9166ac55aa84aea9e3816ccef7c4b
Tyler M <t_mckenzie@outlook.com>
2022-12-19 12:44:31 -0600

    Updated Projects

--- src/components/GitCardData.js
+++ src/components/GitCardData.js
@@ -1,28 +1,30 @@
-import GHLogo from "../assets/GitHubLogo.jpg";
+import GHLogo from '../assets/GitHubLogo.jpg';
 
 const GithubCardData = [
-    {
-        imgcard: GHLogo,
-        title: "Fake University",
-        text: "ASP.net Core assignment that allows you to register for courses," + 
-        "as a student, or as a professor. Includes DB functionality and was fun for me to do.",
-        visit: "https://github.com/TyMckenz/ASPNetCore"
-    },
-    {
-        imgcard: GHLogo,
-        title: "Currency Converter",
-        text: "My first Angular project that uses a constantly updated API to accurately convert from" + 
-        "one currency to another.",
-        visit: "https://github.com/TyMckenz/currency-converter"
-    },
-    {
-        imgcard: GHLogo,
-        title: "Counter",
-        text: "My first project done in React, using some external button styles to create" + 
-        "a mock shopping page where you can add, remove ad delete products from your cart.",
-        visit: "https://github.com/TyMckenz/counterapp"
-    }
-
-]
+  {
+    imgcard: GHLogo,
+    title: 'Fake University',
+    text:
+      'ASP.net Core assignment that allows you to register for courses,' +
+      'as a student, or as a professor. Includes DB functionality and was fun for me to do.',
+    visit: 'https://github.com/TyMckenz/ASPNetCore',
+  },
+  {
+    imgcard: GHLogo,
+    title: 'Tailwind Sneaker Store',
+    text:
+      'Fake sneaker store using Next.JS, MongoDB and Tailwind CSS' +
+      'Store Name: Cheeky Sneakies.',
+    visit: 'https://github.com/TyMckenz/tailwind-sneaker-store',
+  },
+  {
+    imgcard: GHLogo,
+    title: 'Python Slot Machine',
+    text:
+      'My first project done with Python, a sadly punishing' +
+      'slot machine that barely lets you win',
+    visit: 'https://github.com/TyMckenz/firstPythonProject',
+  },
+];
 
 export default GithubCardData;
