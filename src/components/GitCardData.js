4d07120ba7b039d452f58e9907a3bc3cf2d9141f
Tyler M <t_mckenzie@outlook.com>
2022-12-19 12:48:23 -0600

    Spaces

--- src/components/GitCardData.js
+++ src/components/GitCardData.js
@@ -13,7 +13,7 @@ const GithubCardData = [
     imgcard: GHLogo,
     title: 'Tailwind Sneaker Store',
     text:
-      'Fake sneaker store using Next.JS, MongoDB and Tailwind CSS' +
+      'Fake sneaker store using Next.JS, MongoDB and Tailwind CSS ' +
       'Store Name: Cheeky Sneakies.',
     visit: 'https://github.com/TyMckenz/tailwind-sneaker-store',
   },
@@ -21,7 +21,7 @@ const GithubCardData = [
     imgcard: GHLogo,
     title: 'Python Slot Machine',
     text:
-      'My first project done with Python, a sadly punishing' +
+      'My first project done with Python, a sadly punishing ' +
       'slot machine that barely lets you win',
     visit: 'https://github.com/TyMckenz/firstPythonProject',
   },
