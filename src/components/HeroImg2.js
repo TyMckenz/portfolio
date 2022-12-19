6fa332c5912bcebc5cfe6c6c9d736d8a967a82cb
Tyler M <t_mckenzie@outlook.com>
2022-12-19 12:52:14 -0600

    Updated Projects desc

--- src/components/HeroImg2.js
+++ src/components/HeroImg2.js
@@ -1,21 +1,21 @@
-import "./HeroImg2Style.css";
-import ProjectPage from "../assets/largeIcetopes.jpg"
-import React from 'react'
+import './HeroImg2Style.css';
+import ProjectPage from '../assets/largeIcetopes.jpg';
+import React from 'react';
 
 const HeroImg2 = () => {
   return (
     <div className="hero-img">
-        <div className="mask">
-            <img className="project-page" src={ProjectPage} alt="ProjectPage"/>
-        </div>
-        <div className="mid">
-            <h1>Projects</h1>
-            <h3>Includes a link to Github</h3>
-            <h4>Content includes C#/ASP.net, Angular, React, and some Javascript.</h4>
-            <h4>Content on Java to be added.</h4>
-        </div>
+      <div className="mask">
+        <img className="project-page" src={ProjectPage} alt="ProjectPage" />
+      </div>
+      <div className="mid">
+        <h1>Projects</h1>
+        <h3>Includes links to Github Projects</h3>
+        <h4>Github includes C#/ASP.net, Angular, React, Next.JS, </h4>
+        <h4>Java, and some Python</h4>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroImg2
+export default HeroImg2;
