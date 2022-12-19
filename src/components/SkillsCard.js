4f601863dec34b3e4a2f5ed40a734d5e589892fe
Tyler M <t_mckenzie@outlook.com>
2022-12-19 12:37:15 -0600

    Small Skills Update

--- src/components/SkillsCard.js
+++ src/components/SkillsCard.js
@@ -1,44 +1,44 @@
-import "./SkillsCardStyle.css";
-import React from 'react'
+import './SkillsCardStyle.css';
+import React from 'react';
 
 const SkillsCard = () => {
   return (
     <div className="cards">
-        <div className="card-container">
-            <div>
-                <h1>Personal Skills</h1>
-                <p>Quick/Eager Learner</p>
-                <p>Personable</p>
-                <p>Strong Work Ethic</p>
-                <p>Good Teammate</p>
-                <p>Attention To Detail</p>
-            </div>
-            <div>
-                <h1>Technical Skills</h1>
-                <p>HTML, CSS, JS</p>
-                <p>Java</p>
-                <p>C#/ASP.net Core</p>
-                <p>SQL</p>
-                <p>Python</p>
-                <p>Angular/React</p>
-            </div>
-            <div>
-                <h1>Transferable Skills</h1>
-                <p>Customer Service</p>
-                <p>Strategic Planning</p>
-                <p>Inventory Control</p>
-                <p>Instructing</p>
-                <p>Visual Merchendising</p>
-            </div>
-            <div>
-                <h1>Physical Skills</h1>
-                <p>Fitness/Training</p>
-                <p>Manual Dexterity</p>
-                <p>Focus Under Pressure</p>
-            </div>
+      <div className="card-container">
+        <div>
+          <h1>Personal Skills</h1>
+          <p>Quick/Eager Learner</p>
+          <p>Personable</p>
+          <p>Strong Work Ethic</p>
+          <p>Good Teammate</p>
+          <p>Attention To Detail</p>
         </div>
+        <div>
+          <h1>Technical Skills</h1>
+          <p>HTML, CSS, JS</p>
+          <p>Java</p>
+          <p>C#/ASP.net Core</p>
+          <p>SQL/MongoDB</p>
+          <p>Python</p>
+          <p>Angular/React/NextJS</p>
+        </div>
+        <div>
+          <h1>Transferable Skills</h1>
+          <p>Creative Problem Solving</p>
+          <p>Strategic Planning</p>
+          <p>Leading/Teaching</p>
+          <p>Customer Service</p>
+          <p>Product Knowledge</p>
+        </div>
+        <div>
+          <h1>Physical Skills</h1>
+          <p>Fitness/Training</p>
+          <p>Manual Dexterity</p>
+          <p>Focus Under Pressure</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SkillsCard
+export default SkillsCard;
