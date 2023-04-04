0632a08325d4b0cdbe0b0d674ba164519af54348
Tyler M <t_mckenzie@outlook.com>
2023-04-04 12:45:06 -0500

    Small update changes

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,26 +1,34 @@
-import "./HeroImgStyle.css";
-import MainPage from "../assets/largeSpace.jpg";
+import './HeroImgStyle.css';
+import MainPage from '../assets/largeSpace.jpg';
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const HeroImg = () => {
   return (
     <div className="hero">
       <div className="mask">
-        <img className="main-page" src={MainPage} alt="MainPage"/>
+        <img className="main-page" src={MainPage} alt="MainPage" />
       </div>
       <div className="content">
         <h1>Tyler Mckenzie</h1>
-        <p>New graduate from Full Stack Web Development.</p>
+        <p>Full Stack Web Developer.</p>
         <div>
-            <Link to="/skills" className="btn">Skills</Link>
-            <Link to="/education" className="btn btn-light">Education</Link>
-            <Link to="/experience" className="btn">Experience</Link>
-            <Link to="/projects" className="btn btn-light">Projects</Link>
+          <Link to="/skills" className="btn">
+            Skills
+          </Link>
+          <Link to="/education" className="btn">
+            Education
+          </Link>
+          <Link to="/experience" className="btn">
+            Experience
+          </Link>
+          <Link to="/projects" className="btn">
+            Projects
+          </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroImg
+export default HeroImg;
